d5ec47a8627327f7b7a2087e8dfcca27d76ae28d
Jessica Turner <jmcclung3509@gmail.com>
2024-04-02 06:44:24 -0700

    Update db.ts

--- src/db.ts
+++ src/db.ts
@@ -5,9 +5,6 @@ const connection_string = process.env.MONGO_URI;
 
 const mongoose = require('mongoose'); 
 
-
-const url = 'mongodb+srv://jmcclung3509:o3vs1zCyuoYsVzDc@cluster0.do8pzck.mongodb.net/';
-
 const connectionParams = {
   useNewUrlParser: true,
   useUnifiedTopology: true,
